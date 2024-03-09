558d34c58a2cc1af2f90a6670f7375125925d3df
SumanM1234 <sumanmallick121@gmail.com>
2024-03-09 21:19:36 +0530

    Basic setup without CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>BMI Calculator</h2>
+      <form>
+        <div>
+          <label>Weight (lbs)</label>
+          <input type="text" placeholder="Enter weight" value={weight} />
+        </div>
+        <div>
+          <label>Height (in)</label>
+          <input type="text" placeholder="Enter height" value={height} />
+        </div>
+        <div>
+          <button type='submit'>Submit</button>
+          <button>Reload</button>
+        </div>
+      </form>
     </div>
   );
 }
